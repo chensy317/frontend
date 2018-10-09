4a57155be5c5575e4b08bf7d3cadc708dbc53d41
Siyuan Chen <schen01@utbm.fr>
2018-10-09 12:18:09 +0200

    ke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pizza } from 'src/app/pizza';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pizzaParent: Array<Pizza> = [
+    {name: "reine", price: 10, url: "https://www.atelierdeschefs.com/media/recette-e30299-pizza-pepperoni-tomate-mozza.jpg"},
+    {name: "marguerite", price: 12, url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/280px-Eq_it-na_pizza-margherita_sep2005_sml.jpg"}
+  ]
+  
+
 }
---
+++ src/app/pizza.ts
@@ -0,0 +1,6 @@
+export class Pizza {
+    id: number
+    name: String
+    price: number
+    url: string
+}
