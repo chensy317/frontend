b2dd5af25d62fbf4df6e948efbf67b267ab91958
chensy317 <43165258+chensy317@users.noreply.github.com>
2018-10-14 21:03:04 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
 
+import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { PizzadetailComponent } from './pizzadetail/pizzadetail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaComponent,
-    PizzaListComponent
+    PizzaListComponent,
+    PizzadetailComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
